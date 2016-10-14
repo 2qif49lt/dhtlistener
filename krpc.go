94c97604a902e9a54b6eaf6fd5aa05188c8aa340
downmooner <11she_232@163.com>
2016-10-14 15:13:30 +0800

    1

--- krpc.go
+++ krpc.go
@@ -7,10 +7,10 @@ import (
 )
 
 const (
-	pingNode     = "ping"
-	findNode     = "find_node"
-	getPeers     = "get_peers"
-	announcePeer = "announce_peer"
+	pingType         = "ping"
+	findNodeType     = "find_node"
+	getPeersType     = "get_peers"
+	announcePeerType = "announce_peer"
 )
 
 const (
@@ -75,9 +75,9 @@ type transaction struct {
 
 type transactionManager struct {
 	*sync.RWMutex
-	transactions *syncMap
-	index        *syncMap
-	curTransId   uint64 // MaxInt32
+	transactions *syncMap // transid : transaction
+	index        *syncMap // query type + addr : transaction
+	curTransId   uint64   // MaxInt32
 	queryChan    chan *query
 	dht          *DHT
 }
@@ -107,4 +107,176 @@ func (tm *transactionManager) newTransaction(id string, q *query) *transaction {
 		query:    q,
 		response: make(chan struct{}, tm.dht.Try+1),
 	}
+}
+
+// genIndexKey generates an indexed key which consists of queryType and
+// address.
+func (tm *transactionManager) genIndexKey(queryType, address string) string {
+	return strings.Join([]string{queryType, address}, ":")
+}
+
+// genIndexKeyByTrans generates an indexed key by a transaction.
+func (tm *transactionManager) genIndexKeyByTrans(trans *transaction) string {
+	return tm.genIndexKey(trans.data["q"].(string), trans.tar.addr.String())
+}
+
+// insert adds a transaction to transactionManager.
+func (tm *transactionManager) insert(trans *transaction) {
+	tm.Lock()
+	defer tm.Unlock()
+
+	tm.transactions.Set(trans.id, trans)
+	tm.index.Set(tm.genIndexKeyByTrans(trans), trans)
+}
+
+// delete removes a transaction from transactionManager.
+func (tm *transactionManager) delete(transID string) {
+	v, ok := tm.transactions.Get(transID)
+	if !ok {
+		return
+	}
+
+	tm.Lock()
+	defer tm.Unlock()
+
+	trans := v.(*transaction)
+	tm.transactions.Delete(trans.id)
+	tm.index.Delete(tm.genIndexKeyByTrans(trans))
+}
+
+// len returns how many transactions are requesting now.
+func (tm *transactionManager) len() int {
+	return tm.transactions.Len()
+}
+
+// transaction returns a transaction. keyType should be one of 0, 1 which
+// represents transId and index each.
+func (tm *transactionManager) transaction(key string, keyType int) *transaction {
+
+	sm := tm.transactions
+	if keyType == 1 {
+		sm = tm.index
+	}
+
+	v, ok := sm.Get(key)
+	if !ok {
+		return nil
+	}
+
+	return v.(*transaction)
+}
+
+// getByTransID returns a transaction by transID.
+func (tm *transactionManager) getByTransID(transID string) *transaction {
+	return tm.transaction(transID, 0)
+}
+
+// getByIndex returns a transaction by indexed key.
+func (tm *transactionManager) getByIndex(index string) *transaction {
+	return tm.transaction(index, 1)
+}
+
+// transaction gets the proper transaction with whose id is transId and
+// address is addr.
+func (tm *transactionManager) filterOne(transID string, addr *net.UDPAddr) *transaction {
+
+	trans := tm.getByTransID(transID)
+	if trans == nil || trans.node.addr.String() != addr.String() {
+		return nil
+	}
+
+	return trans
+}
+
+// query sends the query-formed data to udp and wait for the response.
+// When timeout, it will retry `try - 1` times, which means it will query
+// `try` times totally.
+func (tm *transactionManager) query(q *query, try int) {
+	transID := q.data["t"].(string)
+	trans := tm.newTransaction(transID, q)
+
+	tm.insert(trans)
+	defer tm.delete(trans.id)
+
+	success := false
+	for i := 0; i < try; i++ {
+		if err := send(tm.dht, q.tar.addr, q.data); err != nil {
+			break
+		}
+
+		select {
+		case <-trans.response:
+			success = true
+			break
+		case <-time.After(time.Second * 15):
+		}
+	}
+
+	if !success && q.tar.id != nil {
+		tm.dht.rt.Remove(q.tar.id)
+	}
+}
+
+// run starts to listen and consume the query chan.
+func (tm *transactionManager) run() {
+	var q *query
+
+	for {
+		select {
+		case q = <-tm.queryChan:
+			go tm.query(q, tm.dht.Try)
+		}
+	}
+}
+
+// sendQuery send query-formed data to the chan.
+func (tm *transactionManager) sendQuery(no *node, queryType string, a map[string]interface{}) {
+
+	// If the target is self, then stop.
+	if (no.id != nil && no.id.RawString() == tm.dht.me.id.RawString()) ||
+		tm.getByIndex(tm.genIndexKey(queryType, no.addr.String())) != nil {
+		return
+	}
+
+	data := makeQuery(tm.genTransID(), queryType, a)
+	tm.queryChan <- &query{
+		node: no,
+		data: data,
+	}
+}
+
+// ping sends ping query to the chan.
+func (tm *transactionManager) ping(no *node) {
+	tm.sendQuery(no, pingType, map[string]interface{}{
+		"id": tm.dht.id(no.id.RawString()),
+	})
+}
+
+// findNode sends find_node query to the chan.
+func (tm *transactionManager) findNode(no *node, target string) {
+	tm.sendQuery(no, findNodeType, map[string]interface{}{
+		"id":     tm.dht.me.id.RawString(),
+		"target": target,
+	})
+}
+
+// getPeers sends get_peers query to the chan.
+func (tm *transactionManager) getPeers(no *node, infoHash string) {
+	tm.sendQuery(no, getPeersType, map[string]interface{}{
+		"id":        tm.dht.me.id.RawString(),
+		"info_hash": infoHash,
+	})
+}
+
+// announcePeer sends announce_peer query to the chan.
+func (tm *transactionManager) announcePeer(
+	no *node, infoHash string, impliedPort, port int, token string) {
+
+	tm.sendQuery(no, announcePeerType, map[string]interface{}{
+		"id":           tm.dht.me.id.RawString(),
+		"info_hash":    infoHash,
+		"implied_port": impliedPort,
+		"port":         port,
+		"token":        token,
+	})
 }
